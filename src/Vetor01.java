ea284b1796cf4e20cc4145603a92ea68d599fbd9
Lcs1993 <ldacosta461@gmail.com>
2021-10-05 08:42:51 -0300

    exemplo de vetor

---
+++ src/Vetor01.java
@@ -0,0 +1,12 @@
+
+public class Vetor01 {
+
+	public static void main(String[] args) {
+		int vetor[]= {3,9,20,18,4};
+		for (int c=0;c<=vetor.length-1;c++) {
+			System.out.println("a posição "+c+" tem o valor "+vetor[c]);
+		}
+
+	}
+
+}
